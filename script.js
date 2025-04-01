cdaec5a15a23a8c026d2249d80879d5f9eac7329
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-04-01 11:21:48 -0400

    feat(calculator): add basic arithmetic functions
    Implement the add, subtract, multiply, and divide functions
    within the calculator object. This establishes the core
    functionality for performing calculations.

---
+++ script.js
@@ -0,0 +1,14 @@
+const calculator = {
+  add: function (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+  },
+  subtract: function (firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+  },
+  multiply: function (firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+  },
+  divide: function (firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+  },
+};
