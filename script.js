51a47f85ca1e9d7f4d0bc6254c691c750eafede4
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-04-01 11:55:17 -0400

    feat: add operate function and operand storage
    Adds an `operate` function to the calculator object. This function
    uses the stored `firstNumber`, `secondNumber`, and `operator`
    to perform the corresponding arithmetic operation by calling the
    appropriate method (add, subtract, multiply, divide).
    
    Also introduces `firstNumber`, `secondNumber`, and `operator`
    properties to the calculator object to store the operands and the
    selected operation.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const calculator = {
+  firstNumber: 3,
+  secondNumber: 2,
+  operator: "+",
   add: function (firstNumber, secondNumber) {
     return firstNumber + secondNumber;
   },
@@ -10,5 +13,24 @@ const calculator = {
   },
   divide: function (firstNumber, secondNumber) {
     return firstNumber / secondNumber;
+  },
+  operate: function (firstNumber, secondNumber, operator) {
+    switch (operator) {
+      case "+":
+        return this.add(firstNumber, secondNumber);
+        break;
+      case "-":
+        return this.subtract(firstNumber, secondNumber);
+        break;
+      case "*":
+        return this.multiply(firstNumber, secondNumber);
+        break;
+      case "/":
+        return this.divide(firstNumber, secondNumber);
+        break;
+
+      default:
+        break;
+    }
   },
 };
