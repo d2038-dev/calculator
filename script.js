1228b871d49c0b67096348dfc96ffb4dc1d4d89a
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2026-04-23 15:11:53 -0400

    refactor(calc): implement number input handling and display updates
    Add event listeners to number buttons to capture user input and
    synchronize the calculator state with the UI display.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const calculator = {
-  firstNumber: null,
-  secondNumber: null,
+  firstNumber: "0",
+  secondNumber: "",
   operator: null,
   add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -32,4 +32,29 @@ const calculator = {
         break;
     }
   }
+};
+
+const numberBtnGroup = document.querySelectorAll('.calculator__button--type-number');
+const numberDisplay = document.querySelector('.calculator__number-line');
+
+numberBtnGroup.forEach((button) => {
+  button.addEventListener("click", update);
+});
+
+function update(e) {
+  updateState(e);
+  updateDisplay();
+};
+
+function updateState(e) {
+  const value = e.target.textContent;
+  if (calculator.firstNumber === "0") {
+    calculator.firstNumber = value;
+  } else {
+    calculator.firstNumber += value;
+  }
+};
+
+function updateDisplay() {
+  numberDisplay.textContent = calculator.firstNumber;
 };
